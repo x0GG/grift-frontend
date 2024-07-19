1c6b2ed75ae3293466c52b140dca8c019f9dcb3f
0xGG <x0GG@proton.me>
2024-07-19 22:11:12 +0100

    fix: static removed

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin"
 const withNextIntl = createNextIntlPlugin("./config/i18n.ts")
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    distDir: 'docs'
-}
+const nextConfig = {}
 
 export default withNextIntl(nextConfig)
