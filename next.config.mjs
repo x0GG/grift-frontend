e2c50def0e070d23183281f40e5c420c4ef07e27
0xGG <x0GG@proton.me>
2024-07-19 22:36:41 +0100

    fix: upd next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,9 @@ const withNextIntl = createNextIntlPlugin("./config/i18n.ts")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: '/grift-frontend',
+    reactStrictMode: true
 }
 
 export default withNextIntl(nextConfig)
