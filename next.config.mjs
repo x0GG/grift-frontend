9355ce465b2e0fc6f38fc1d9d55c4c4e54672d8f
0xGG <x0GG@proton.me>
2024-09-13 21:13:00 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,13 @@ import createNextIntlPlugin from "next-intl/plugin"
 const withNextIntl = createNextIntlPlugin("./config/i18n.ts")
 
 /** @type {import('next').NextConfig} */
-const nextConfig = process.env.NODE_ENV === 'development' ? {
+const nextConfig = process.env.NODE_ENV === 'production' ? {
     output: 'export',
-    basePath: '',
+    basePath: '/grift-frontend',
     reactStrictMode: false
 } : {
     output: 'export',
-    basePath: '/grift-frontend',
+    basePath: '',
     reactStrictMode: false
 }
 
